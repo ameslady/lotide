94de89058eb6d2e326a3fc3c02e71412f1d32471
Amy Martin <amymariemart@gmail.com>
2022-04-06 17:31:01 -0700

    Updated countOnly to effectively tally truthy names within object

--- countOnly.js
+++ countOnly.js
@@ -12,21 +12,15 @@ const assertEqual = function(actual, expected) {
 // allItems: an array of strings that we need to look through
 // itemsToCount: an object specifying what to count
 const countOnly = function(allItems, itemsToCount) {
-  let newObj = {
-    "Fang": 0,
-    "Jason": 0,
-  };
-  // console.log(itemsToCount['']);
+  let validItems = {};
 
-  for (let name of allItems){
+  for (let name of allItems) {
     if (itemsToCount[name] === true) {
-      //console.log(itemsToCount[name]);
-      newObj[name] += 1;
-      //console.log(newObj);
-    }  
+      validItems[name] = validItems[name] + 1 || 1;
+    }
   }
-//The function should report back how many instances of each string were found in the allItems array of strings.
-  return newObj;
+  //reports back how many instances of each string were found in the allItems array of strings
+  return validItems;
 };
 
 // test cases
@@ -43,7 +37,6 @@ const firstNames = [
 ];
 
 const result1 = countOnly(firstNames, { "Jason": true, "Karima": true, "Fang": true, "Agouhanna": false });
-//console.log(result1);
 
 assertEqual(result1["Jason"], 1);
 assertEqual(result1["Karima"], undefined);
