77a1998c7c95894391126a2038519d1073f19474
Amy Martin <amymariemart@gmail.com>
2022-04-06 20:47:16 -0700

    Updated test cases for findKeyByValue.js

--- findKeyByValue.js
+++ findKeyByValue.js
@@ -8,10 +8,12 @@ const assertEqual = function(actual, expected) {
   }
 };
 
-// scans the object and returns the first key that contains the given value. 
+// scans the object and returns the first key that contains the given value.
 // if no key with that given value is found, then it should return undefined.
 const findKeyByValue = function(object, value) {
-  for (let key in object) {
+  let allKeys = Object.keys(object);
+
+  for (const key of allKeys) {
     if (object[key] === value) {
       return key;
     }
@@ -19,7 +21,7 @@ const findKeyByValue = function(object, value) {
 };
 
 // test cases
-const bestTVShowsByGenre = { 
+const bestTVShowsByGenre = {
   sci_fi: "The Expanse",
   comedy: "Brooklyn Nine-Nine",
   drama:  "The Wire"
@@ -27,4 +29,6 @@ const bestTVShowsByGenre = {
 
 //console.log(findKeyByValue(bestTVShowsByGenre, "The Wire"));
 assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
+assertEqual(findKeyByValue(bestTVShowsByGenre, "Brooklyn Nine-Nine"), "comedy");
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Expanse"), "comedy");
 assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
