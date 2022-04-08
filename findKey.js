0bcf22ac27bd6c87328a9bcbe70a10b2ae96ad4e
Amy Martin <amymariemart@gmail.com>
2022-04-07 19:28:00 -0700

    findKeys function passes all test cases

--- findKey.js
+++ findKey.js
@@ -26,13 +26,13 @@ const findKey = function(object, callback){
 };
 
 
-// test cases
-console.log(findKey({
+// test case
+assertEqual(findKey({
   "Blue Hill": { stars: 1 },
   "Akaleri":   { stars: 3 },
-  "noma":      { stars: 5 },
+  "noma":      { stars: 2 },
   "elBulli":   { stars: 3 },
-  "Ora":       { stars: 9 },
+  "Ora":       { stars: 2 },
   "Akelarre":  { stars: 3 }
-}, x => x.stars === 2)); // => "noma"
+}, x => x.stars === 2), "noma");
 
