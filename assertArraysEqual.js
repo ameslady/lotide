a04a957f9a23db8082bbce336fe787480af58a7f
Amy Martin <amymariemart@gmail.com>
2022-04-08 10:20:39 -0700

    reverting change from test

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -23,5 +23,3 @@ assertArraysEqual([1, 2, 3], [1, 2, 3, 4]); // fail
 assertArraysEqual([1, 2, 3], [3, 2, 3]); // fail
 assertArraysEqual(["1", "2", "3"], ["1", "2", "3"]); // pass
 assertArraysEqual(["1", "2", "3"], ["1", "2", 3]); // fail
-
-//test 
