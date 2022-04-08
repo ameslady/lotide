319ac6ec92d17dfa5b13e99401a0b1af861bc43e
Amy Martin <amymariemart@gmail.com>
2022-04-07 18:08:07 -0700

    Created takeUntil function shell

---
+++ takeUntil.js
@@ -0,0 +1,43 @@
+// to be imported from other files eventually
+const eqArrays = function(arr1, arr2) {
+  if (arr1.length !== arr2.length) return false;
+
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i]) return false;
+  }
+  return true;
+};
+
+// to be imported from other files eventually
+const assertArraysEqual = function(arr1, arr2) {
+  if (eqArrays(arr1, arr2)) {
+    console.log(`✅✅✅ Assertion Passed: ${arr1} === ${arr2}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${arr1} !== ${arr2}`);
+  }
+};
+
+
+// The function will return a "slice of the array with elements taken from the beginning." 
+// It should keep going until the callback/predicate returns a truthy value.
+const takeUntil = function(array, callback) {
+  console.log(array);
+  console.log(callback);
+}
+
+
+// test cases
+const data1 = [1, 2, 5, 7, 2, -1, 2, 4, 5];
+const results1 = takeUntil(data1, x => x < 0);
+//console.log(results1);
+
+console.log('---');
+
+const data2 = ["I've", "been", "to", "Hollywood", ",", "I've", "been", "to", "Redwood"];
+const results2 = takeUntil(data2, x => x === ',');
+//console.log(results2);
+
+// Expected output:
+// [ 1, 2, 5, 7, 2 ]
+// --
+// [ 'I\'ve', 'been', 'to', 'Hollywood' ]
