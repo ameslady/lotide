47f33bc1835e01cf5b265c36b0a21b737f3fbc2d
Amy Martin <amymariemart@gmail.com>
2022-04-07 19:29:41 -0700

    Ensure findKeys.js passed eslint rules

--- findKey.js
+++ findKey.js
@@ -8,11 +8,11 @@ const assertEqual = function(actual, expected) {
   }
 };
 
-// scan the object and return the first key for which the callback returns a truthy value. 
+// scan the object and return the first key for which the callback returns a truthy value.
 // if no key is found, then it should return undefined.
-const findKey = function(object, callback){
-   let keys = Object.keys(object);
-   let result;
+const findKey = function(object, callback) {
+  let keys = Object.keys(object);
+  let result;
 
   for (const key of keys) {
     let value = object[key];
@@ -35,4 +35,3 @@ assertEqual(findKey({
   "Ora":       { stars: 2 },
   "Akelarre":  { stars: 3 }
 }, x => x.stars === 2), "noma");
-
